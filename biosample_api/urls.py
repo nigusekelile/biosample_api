a5cb4b2a62a21072e1cfb0b579638372a4fa43e1
Niguse Kelile Lema <nigusmov16@gmail.com>
2025-12-26 15:04:50 +0200

    fixing bugs and updating urls.py

--- biosample_api/urls.py
+++ biosample_api/urls.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from samples.views import BioSampleViewSet, MetadataFieldViewSet, MetadataValueViewSet
 from qc.views import QCCheckViewSet, QCReportViewSet
-from users.views import CustomAuthToken
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register("biosamples", BioSampleViewSet)
-router.register("metadata-fields", MetadataFieldViewSet)
-router.register("metadata-values", MetadataValueViewSet)
-router.register("qc-checks", QCCheckViewSet)
-router.register("qc-reports", QCReportViewSet)
+router.register(r'samples', BioSampleViewSet)
+router.register(r'metadata-fields', MetadataFieldViewSet)
+router.register(r'metadata-values', MetadataValueViewSet)
+router.register(r'qc-checks', QCCheckViewSet)
+router.register(r'qc-reports', QCReportViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
-    path("api/auth/", CustomAuthToken.as_view()),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/token/', obtain_auth_token),
 ]
